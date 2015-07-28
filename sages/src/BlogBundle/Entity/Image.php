b804e28969b8774e1a35ce1c4e15649fbc602c57
mrybnik <rybbow@gmail.com>
2015-07-28 11:31:26 +0200

    Poprawiony model

--- sages/src/BlogBundle/Entity/Image.php
+++ sages/src/BlogBundle/Entity/Image.php
@@ -1,12 +1,6 @@
 <?php
-/**
- * This file is part of the sages package.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
 
-namespace BlogBundle\Entity;
+namespace X\BlogBundle\Entity;
 
 use Symfony\Component\HttpFoundation\File\File;
 
--- sages/src/BlogBundle/Entity/Tag.php
+++ sages/src/BlogBundle/Entity/Tag.php
@@ -6,7 +6,7 @@
  * file that was distributed with this source code.
  */
 
-namespace BlogBundle\Entity;
+namespace X\BlogBundle\Entity;
 
 
 /**
