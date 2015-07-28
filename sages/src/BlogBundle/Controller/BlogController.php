9e918f633ee00f6661225de9ea098e80557a049d
mrybnik <rybbow@gmail.com>
2015-07-28 11:17:54 +0200

    Schemat kontrolera

---
+++ sages/src/BlogBundle/Controller/BlogController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace X\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class BlogController extends Controller
+{
+    public function indexAction()
+    {
+        // listowanie blogów
+    }
+
+    public function showAction()
+    {
+        // listowanie postów dla bloga
+    }
+
+    public function showPostAction()
+    {
+        // widok posta + lista komentarzy
+    }
+
+    public function editAction()
+    {
+        // edycja bloga
+    }
+
+    public function addPost()
+    {
+        // dodawanie posta
+    }
+
+    public function addComment()
+    {
+        // dodawanie komentarzy
+    }
+
+}
